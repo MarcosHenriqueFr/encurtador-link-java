9cafc43fda41f07b1f338d7f6c11275be1410f08
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-11-05 15:55:01 -0300

    feat: adiciona o protótipo da funcionalidade de encurtamento

---
+++ src/main/java/com/example/encurtadorlink/controllers/LinkController.java
@@ -0,0 +1,30 @@
+package com.example.encurtadorlink.controllers;
+
+import com.example.encurtadorlink.dto.LinkCreateDTO;
+import com.example.encurtadorlink.dto.LinkResponseDTO;
+import com.example.encurtadorlink.services.LinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api")
+public class LinkController {
+
+    private final LinkService linkService;
+
+    @Autowired
+    public LinkController(LinkService linkService){
+        this.linkService = linkService;
+    }
+
+    @PostMapping(path = "shorten")
+    public ResponseEntity<LinkResponseDTO> shortenLink(@RequestBody LinkCreateDTO dto){
+        LinkResponseDTO link = linkService.shortenLink(dto);
+        return new ResponseEntity<>(link, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/example/encurtadorlink/services/LinkService.java
@@ -0,0 +1,60 @@
+package com.example.encurtadorlink.services;
+
+import com.example.encurtadorlink.dto.LinkCreateDTO;
+import com.example.encurtadorlink.dto.LinkResponseDTO;
+import com.example.encurtadorlink.mapper.LinkMapper;
+import com.example.encurtadorlink.model.Link;
+import com.example.encurtadorlink.repositories.LinkRepository;
+import com.example.encurtadorlink.util.Base62;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+
+@Service
+public class LinkService {
+    private static final SecureRandom random = new SecureRandom();
+    private final LinkMapper linkMapper;
+    private final LinkRepository linkRepository;
+
+    @Autowired
+    public LinkService(LinkMapper linkMapper, LinkRepository linkRepository){
+        this.linkMapper = linkMapper;
+        this.linkRepository = linkRepository;
+    }
+
+    private boolean isShortCodeAvailable(String shortCode){
+        Link link = linkRepository.findByShortCode(shortCode).orElse(null);
+        return link == null;
+    }
+
+    public LinkResponseDTO shortenLink(LinkCreateDTO dto) {
+        Link link = linkMapper.toEntity(dto);
+
+        // TODO: Futuramente trocar esse tipo de exception
+        String randomShortCode = generateRandomShortCode();
+        if (!isShortCodeAvailable(randomShortCode)){
+            throw new RuntimeException("Esse shortcode já foi gerado!");
+        }
+
+        link.setShortCode(randomShortCode);
+        link.setActive(true);
+        link.setUser(null); //TODO: Mudar depois da mecânica de login de usuário
+        link.setCreationDate(LocalDateTime.now());
+
+        saveLink(link);
+
+        return linkMapper.fromEntity(link);
+    }
+
+    // TODO: Implementar o log do sistema aqui
+    private void saveLink(Link link){
+        linkRepository.save(link);
+    }
+
+    private String generateRandomShortCode(){
+        long randomNumber = Math.abs(random.nextLong() % 100_000_000_000L);
+        return Base62.encode(randomNumber);
+    }
+}
