321bda853552d27d248bde5210abea0e9da30740
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-11-05 21:33:31 -0300

    feat: cria um controller específico para o redirecionamento

---
+++ src/main/java/com/example/encurtadorlink/controllers/RedirectController.java
@@ -0,0 +1,31 @@
+package com.example.encurtadorlink.controllers;
+
+import com.example.encurtadorlink.dto.LinkResponseDTO;
+import com.example.encurtadorlink.services.LinkService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+public class RedirectController {
+
+    private final LinkService linkService;
+
+    public RedirectController(LinkService linkService){
+        this.linkService = linkService;
+    }
+
+    @GetMapping(path = "{shortCode}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortCode){
+        // TODO: Colocar um exception handler para tratar a exceção
+        String originalUrl = linkService.resolveShortCode(shortCode);
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .location(URI.create(originalUrl))
+                .build();
+    }
+}
