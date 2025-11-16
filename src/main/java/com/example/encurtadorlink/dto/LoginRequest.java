d61401b2c35e438fccfafe7b820d27963d0f3758
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-11-15 21:31:43 -0300

    feat: adiciona um dto para o request de login

---
+++ src/main/java/com/example/encurtadorlink/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.example.encurtadorlink.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String password
+) { }
